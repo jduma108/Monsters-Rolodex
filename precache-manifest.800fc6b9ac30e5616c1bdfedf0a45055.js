4028a379a526288dc1c5a3dc5b248e5b3ec83e3a
Jillian Duma <jduma108@gmail.com>
2020-01-06 15:21:30 -0600

    Updates

---
+++ precache-manifest.800fc6b9ac30e5616c1bdfedf0a45055.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c7f192c710e4f38f1c5fb7a9c34aeaa",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b667cba188d4dfbb4328",
+    "url": "/monsters-rolodex/static/css/main.4da3f18b.chunk.css"
+  },
+  {
+    "revision": "4c43680a4c1defaef845",
+    "url": "/monsters-rolodex/static/js/2.ec8ba6cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.ec8ba6cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b667cba188d4dfbb4328",
+    "url": "/monsters-rolodex/static/js/main.c2a0f75a.chunk.js"
+  },
+  {
+    "revision": "8f564ee1c346f305c76d",
+    "url": "/monsters-rolodex/static/js/runtime-main.17d07582.js"
+  }
+]);
